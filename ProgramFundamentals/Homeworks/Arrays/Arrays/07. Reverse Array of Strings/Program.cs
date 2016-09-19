bed1429b7064e1e8aef5b0b981a495a021ef6693
Boyan Medarski <boyanmedarski@mail.bg>
2016-09-19 10:13:25 +0300

    Add files via upload

---
+++ ProgramFundamentals/Homeworks/Arrays/Arrays/07. Reverse Array of Strings/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+            Console.WriteLine(String.Join(" ", input.Reverse()));
+        }
+    }
