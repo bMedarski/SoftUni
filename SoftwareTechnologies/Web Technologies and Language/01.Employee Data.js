1f768f57e95e3497dfef87836322a527cb1ecb14
Boyan Medarski <boyanmedarski@mail.bg>
2016-10-27 15:27:28 +0300

    Homework HTML-CSS lab

---
+++ SoftwareTechnologies/Web Technologies and Language/01.Employee Data.js
@@ -0,0 +1,17 @@
+function solve(args){
+
+    let firstName = args[0];
+    let secondName = args[1];
+    let age = args[2];
+    let gender = args[3];
+    let id = args[4];
+    let employeeNumber = args[5];
+    console.log("First name: "+firstName);
+    console.log("Last name: "+secondName);
+    console.log("Age: "+age);
+    console.log("Gender: "+gender);
+    console.log("Personal ID: "+id);
+    console.log("Unique Employee number: "+employeeNumber);
+
+
+}
